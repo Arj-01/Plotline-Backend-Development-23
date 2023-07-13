f27c425ed349dc4265fab7ef3df0300505337e1b
ARJIT GIRI <84219817+Arj-01@users.noreply.github.com>
2023-07-13 14:24:48 +0530

    items router updated

--- routes/itemsRoutes.js
+++ routes/itemsRoutes.js
@@ -5,14 +5,17 @@ const router = express.Router();
 
 // testing of this API using browser done //
 
-router.get('/', (req, res) => {
-    res.status(200).json({
-        msg: "fetching all the products and services here"
-    })
-    console.log("Welcone to the catalog page");
+router.get('/', async (req, res) => {
+    try {
+
+        const products = await Product.find();
+        const services = await Service.find();
+    
+        res.status(200).json({ products, services });
+      } catch (error) {
+        res.status(500).json({ message: 'An error occurred' });
+      }
 });
 
 
 module.exports = router;
-
-
