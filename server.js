5c3ebb8cd47f9c2225979852f10f0663e04b5401
ARJIT GIRI <84219817+Arj-01@users.noreply.github.com>
2023-07-13 12:22:27 +0530

    mongoose connection

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 8001;
 
 
 
+// connecting mongodb remotely with the current project in VS-CODE using string URL//
+const dbUrl = "mongodb+srv://aarjitgiri:billingservice@billingcluster.hwne6nc.mongodb.net/"; 
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+  
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 
+
+
+// server listening at port 8001 //
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
-  });
+});
 
