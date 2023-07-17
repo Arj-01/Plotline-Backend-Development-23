06b3c1a3bf4df7903ca1ac6731cbe51e8423a02b
ARJIT GIRI <84219817+Arj-01@users.noreply.github.com>
2023-07-17 11:50:36 +0530

    removing from cart API

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -74,7 +74,7 @@ router.post("/:type/:itemId", authenticateToken, async (req, res) => {
 
     // console.log("2");
 
-    res.status(200).json({ message: "Item added to the cart", totalPrice });
+    res.status(200).json({ message: "Item added to the cart", cart });
   } catch (error) {
     res.status(500).json({ message: "An error occurred", error });
   }
@@ -85,7 +85,49 @@ router.post("/:type/:itemId", authenticateToken, async (req, res) => {
 
 
 // Removing an item from the cart
-router.delete("/:type/:itemId", authenticateToken, async (req, res) => {});
+router.delete("/:type/:itemId", authenticateToken, async (req, res) => {
+
+  try{
+
+    const userId = req.user.userId;
+    const { type, itemId } = req.params;
+
+    const cart = await Cart.findOne({ userId });
+
+    if (!cart) {
+      return res.status(404).json({ message: 'Cart not found' });
+    }
+
+    let items;
+    if (type === 'product') {
+      items = cart.products;
+    } else if (type === 'service') {
+      items = cart.services;
+    } else {
+      return res.status(400).json({ message: 'Invalid item type' });
+    }
+
+    ///
+    const updatedItems = items.filter((item) => item.item.toString() !== itemId);
+
+    if (type === 'product') {
+      cart.products = updatedItems;
+    } else if (type === 'service') {
+      cart.services = updatedItems;
+    }
+
+    await cart.save();
+
+    res.status(200).json({ message: 'Item removed from the cart', cart});
+  } 
+   
+  catch (error) {
+    res.status(500).json({ message: 'An error occurred' });
+  }
+
+});
+
+
 
 // Clearing the cart
 router.delete("/", authenticateToken, async (req, res) => {
@@ -94,7 +136,7 @@ router.delete("/", authenticateToken, async (req, res) => {
 
     await Cart.findOneAndDelete({ userId });
 
-    res.status(200).json({ message: "Cart cleared successfully" });
+    res.status(200).json({ message: "Cart cleared successfully", Cart});
   } catch (error) {
     res.status(500).json({ message: "An error occurred" });
   }
